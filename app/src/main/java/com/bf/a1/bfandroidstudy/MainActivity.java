d18e1278d3bfe634a712bd84737618c1e1230780
BFAlex <838318741@qq.com>
2017-12-06 09:45:33 +0800

    Initial commit

---
+++ app/src/main/java/com/bf/a1/bfandroidstudy/MainActivity.java
@@ -0,0 +1,13 @@
+package com.bf.a1.bfandroidstudy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
